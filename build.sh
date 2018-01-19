08926675cb221b02cd45d4df36489f3f1e951e03
devpan <admin@xxblog.cn>
2018-01-19 18:16:14 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker-compose down \
-        && git pull origin master \
+git pull origin master \
         && docker-compose build \
-        && docker-compose up -d
+        && docker-compose restart -d
